400922bb2c48cbf1dfc772ae190a301692d366db
DhanuBoi <151012150+DhanuBOI28@users.noreply.github.com>
2024-06-18 08:40:32 +0530

    Update AbstractServer.java
    matched AbstractServer from main BruhNet project

--- Files/BruhNet/Base/Abstracts/AbstractServer.java
+++ Files/BruhNet/Base/Abstracts/AbstractServer.java
@@ -1,1 +1,25 @@
 
+import java.io.Serializable;
+
+public abstract class AbstractServer {
+    protected int port;
+    protected int maxClients;
+    protected int timeout;
+    protected boolean running;
+
+    public AbstractServer(int port, int maxClients, int timeout) {
+        this.port = port;
+        this.maxClients = maxClients;
+        this.timeout = timeout;
+        this.running = false;
+    }
+
+    public abstract void start();
+    public abstract void stop();
+    public abstract void acceptClient(Object clientInfo);
+    public abstract void removeClient(Object clientInfo);
+    public abstract void sendObject(Object clientInfo, Serializable obj) throws Exception;
+    public abstract void broadcast(Serializable obj) throws Exception;
+    public abstract boolean isClientConnected(Object clientInfo);
+    public abstract int getClientCount();
+}
